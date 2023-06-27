2501a208102b7196558335235763815f0f165cc0
Oluwatobi Adegelu <adeniyi.adegelu@gmail.com>
2023-06-27 23:14:38 +0100

    API is been used to fetch data from the server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import {useEffect} from 'react'
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=57ad6176'
+
+//57ad6176
+//http://www.omdbapi.com/?i=tt3896198&apikey=57ad6176
 
 export default function App() {
+    const searchMovies = async (title) => {
+        const res = await fetch(`${API_URL}&s=${title}`)
+        const data = await res.json();
+        console.log(data)
+    
+    }
+    useEffect (() => {
+        searchMovies('Superman');
+    }, [])
   return (
     <div>
       <h1>This is the New App</h1>
     </div>
   )
 }
-
-
